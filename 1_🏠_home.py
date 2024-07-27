6cbfbd942cbe633adf73e151b69b8c778ed8c816
ericluso <ericlopesob@gmail.com>
2024-07-27 15:45:31 -0300

    Update 1_🏠_home.py

--- 1_🏠_home.py
+++ 1_🏠_home.py
@@ -1,23 +1,25 @@
 import streamlit as st
 import webbrowser
 import pandas as pd
-from datetime import datetime 
-import openai
+from datetime import datetime
 
 if "data" not in st.session_state:
-    df_data = pd.read_csv("datasets/CLEAN_FIFA23_official_data.csv", index_col=0)
+    df_data = pd.read_csv(
+        "datasets/CLEAN_FIFA23_official_data.csv", index_col=0)
     df_data = df_data[df_data["Contract Valid Until"] >= datetime.today().year]
     df_data = df_data[df_data["Value(£)"] > 0]
     df_data = df_data.sort_values(by="Overall", ascending=False)
     st.session_state["data"] = df_data
 
 st.markdown("# FIFA23 OFFICIAL DATASET! ⚽️")
-st.sidebar.markdown("Desenvolvido por [Asimov Academy](https://asimov.academy)")
+st.sidebar.markdown(
+    "Desenvolvido por [Asimov Academy](https://asimov.academy)")
 
 
 btn = st.button("Acesse os dados no Kaggle")
 if btn:
-    webbrowser.open_new_tab("https://www.kaggle.com/datasets/kevwesophia/fifa23-official-datasetclean-data")
+    webbrowser.open_new_tab(
+        "https://www.kaggle.com/datasets/kevwesophia/fifa23-official-datasetclean-data")
 
 st.markdown(
     """
